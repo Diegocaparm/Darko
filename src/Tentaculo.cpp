3ddf28759865fdd8ce062dc005c304a9855a0e34
davidapupm <72233683+davidapupm@users.noreply.github.com>
2021-06-13 22:36:44 +0200

    Update Tentaculo.cpp

--- src/Tentaculo.cpp
+++ src/Tentaculo.cpp
@@ -26,22 +26,22 @@ void Tentaculo::dibuja() {
 	glRotatef(a, 0, 1, 0);
 	glutSolidCylinder(longitud / 6, longitud, 30, 30);
 	glutSolidSphere(longitud / 6, 30, 30);
-	
+
 	glTranslatef(0, 0, longitud);
 	glRotatef(a, 0, 1, 0);
 	glutSolidCylinder(longitud / 7, longitud, 30, 30);
 	glutSolidSphere(longitud / 7, 30, 30);
-	
+
 	glTranslatef(0, 0, longitud);
 	glRotatef(a, 0, 1, 0);
 	glutSolidCylinder(longitud / 8, longitud, 30, 30);
 	glutSolidSphere(longitud / 8, 30, 30);
-	
+
 	//glRotatef(90, 0, 1, 0);
 	glPopMatrix();
 
 	glPushMatrix();
-	glTranslatef(posicion.x, posicion.y, 0);
+	//glTranslatef(posicion.x, posicion.y, 0);
 	glBegin(GL_LINES);
 	glColor3f(1.0f, 1.0f, 1.0f);
 	glVertex3f(hitbox[0].esquina1.x, hitbox[0].esquina1.y, 0);
@@ -63,7 +63,7 @@ void Tentaculo::dibuja() {
 	glVertex3f(hitbox[0].esquina1.x, hitbox[0].esquina1.y, 0);
 	glEnd();
 
-	
+
 	glBegin(GL_LINES);
 	glColor3f(1.0f, 1.0f, 1.0f);
 	glVertex3f(hitbox[1].esquina1.x, hitbox[1].esquina1.y, 0);
@@ -112,8 +112,8 @@ void Tentaculo::mueve(float t) {
 	posicion = posicion + velocidad * t + aceleracion * 0.5f * t * t;
 	velocidad = velocidad + aceleracion * t;
 	velocidad.x = aceleracion.x = 0;
-	
-	int lim=15;
+
+	int lim = 15;
 	if (a > lim)
 		flag = 1;
 	if (a < -lim)
@@ -123,7 +123,7 @@ void Tentaculo::mueve(float t) {
 		a += 0.2f;
 	else
 		a -= 0.2f;
-	
+
 	Vector2D e1, e2, e3, e4;
 	/*float h0 = sqrt(longitud * longitud + longitud * longitud / 36);
 	float beta0 = atan(1 / 6);
@@ -132,10 +132,10 @@ void Tentaculo::mueve(float t) {
 	e3.x = -(longitud / 6) * cos(a * pi / 180);		e3.y = (longitud / 6) * (sin(a * pi / 180));
 	e4.x = (longitud / 6) * cos(a * pi / 180);		e4.y = -(longitud / 6) * (sin(a * pi / 180));*/
 
-	e1.x = -(longitud / 6) + longitud*sin(a * pi / 180);		e1.y = (longitud) * (cos(-a * pi / 180));
-	e2.x = (longitud / 6) + longitud*sin(a * pi / 180);			e2.y = (longitud) * (cos(-a * pi / 180));
-	e3.x = -(longitud / 6) * cos(-a * pi / 180);				e3.y = -(longitud / 6) * (sin(-a * pi / 180));
-	e4.x = (longitud / 6) * cos(-a * pi / 180);					e4.y = (longitud / 6) * (sin(-a * pi / 180));
+	e1.x = -(longitud / 6) + longitud * sin(a * pi / 180) + posicion.x;		e1.y = (longitud) * (cos(-a * pi / 180));
+	e2.x = (longitud / 6) + longitud * sin(a * pi / 180) + posicion.x;			e2.y = (longitud) * (cos(-a * pi / 180));
+	e3.x = -(longitud / 6) * cos(-a * pi / 180) + posicion.x;				e3.y = -(longitud / 6) * (sin(-a * pi / 180));
+	e4.x = (longitud / 6) * cos(-a * pi / 180) + posicion.x;					e4.y = (longitud / 6) * (sin(-a * pi / 180));
 	hitbox[0].setPos(e1, e2, e3, e4);
 	hitbox[0].segmento[0] = { e1.x, e1.y, e2.x, e2.y };
 	hitbox[0].segmento[1] = { e2.x, e2.y, e4.x, e4.y };
@@ -144,29 +144,29 @@ void Tentaculo::mueve(float t) {
 
 
 	Vector2D e11, e21, e31, e41;
-	e11.x = -(longitud / 6) + longitud * sin(3*a * pi / 180);		e11.y = (longitud) * (cos(3*a * pi / 180)) + 2*sin(a * pi / 180)* sin(a * pi / 180);
-	e21.x = (longitud / 6) + longitud * sin(3*a * pi / 180);		e21.y = (longitud) * (cos(3*a * pi / 180)) + 2*sin(a * pi / 180)* sin(a * pi / 180);
-	e31.x = -(longitud / 6) + longitud * sin(a * pi / 180);			e31.y = (longitud) * (cos(-a * pi / 180));
-	e41.x = (longitud / 6) + longitud * sin(a * pi / 180);			e41.y = (longitud) * (cos(-a * pi / 180));
-	Vector2D e111 = { e11.x, e1.y+e11.y}, e211 = { e21.x, e2.y+e21.y };
+	e11.x = -(longitud / 6) + longitud * sin(3 * a * pi / 180);		e11.y = (longitud) * (cos(3 * a * pi / 180)) + 2 * sin(a * pi / 180) * sin(a * pi / 180);
+	e21.x = (longitud / 6) + longitud * sin(3 * a * pi / 180);		e21.y = (longitud) * (cos(3 * a * pi / 180)) + 2 * sin(a * pi / 180) * sin(a * pi / 180);
+	e31.x = -(longitud / 6) + longitud * sin(a * pi / 180) + posicion.x;			e31.y = (longitud) * (cos(-a * pi / 180));
+	e41.x = (longitud / 6) + longitud * sin(a * pi / 180) + posicion.x;			e41.y = (longitud) * (cos(-a * pi / 180));
+	Vector2D e111 = { e11.x + posicion.x, e1.y + e11.y }, e211 = { e21.x + posicion.x, e2.y + e21.y };
 	hitbox[1].setPos(e111, e211, e31, e41);
 	hitbox[1].segmento[0] = { e111.x, e111.y, e211.x, e211.y };
 	hitbox[1].segmento[1] = { e211.x, e211.y, e41.x, e41.y };
 	hitbox[1].segmento[2] = { e31.x, e31.y, e41.x, e41.y };
 	hitbox[1].segmento[3] = { e111.x, e111.y, e31.x, e31.y };
-	
+
 	Vector2D e12, e22, e32, e42;
-	e12.x = -(longitud / 6) + longitud * 2*sin(3.2*a * pi / 180);		e12.y = (longitud) * (cos(a * pi / 180))- 0.5*(cos(a * pi / 180));
-	e22.x = (longitud / 6) + longitud * 2*sin(3.2*a * pi / 180);		e22.y = (longitud) * (cos(a * pi / 180))- 0.5*(cos(a * pi / 180));
-	e32.x = -(longitud / 6) + longitud * sin(3*a * pi / 180);			e32.y = (longitud) * (cos(3*a * pi / 180)) + 2*sin(a * pi / 180)* sin(a * pi / 180);
-	e42.x = (longitud / 6) + longitud * sin(3*a * pi / 180);			e42.y = (longitud) * (cos(3*a * pi / 180)) + 2*sin(a * pi / 180)* sin(a * pi / 180);
-	Vector2D e121 = { e12.x, e111.y + e12.y }, e221 = { e22.x, e211.y + e22.y };
+	e12.x = -(longitud / 6) + longitud * 2 * sin(3.2 * a * pi / 180);		e12.y = (longitud) * (cos(a * pi / 180)) - 0.5 * (cos(a * pi / 180));
+	e22.x = (longitud / 6) + longitud * 2 * sin(3.2 * a * pi / 180);		e22.y = (longitud) * (cos(a * pi / 180)) - 0.5 * (cos(a * pi / 180));
+	e32.x = -(longitud / 6) + longitud * sin(3 * a * pi / 180);			e32.y = (longitud) * (cos(3 * a * pi / 180)) + 2 * sin(a * pi / 180) * sin(a * pi / 180);
+	e42.x = (longitud / 6) + longitud * sin(3 * a * pi / 180);			e42.y = (longitud) * (cos(3 * a * pi / 180)) + 2 * sin(a * pi / 180) * sin(a * pi / 180);
+	Vector2D e121 = { e12.x + posicion.x, e111.y + e12.y }, e221 = { e22.x + posicion.x, e211.y + e22.y };
 	hitbox[2].setPos(e121, e221, e111, e211);
 	hitbox[2].segmento[0] = { e121.x, e121.y, e221.x, e221.y };
 	hitbox[2].segmento[1] = { e221.x, e221.y, e211.x, e211.y };
 	hitbox[2].segmento[2] = { e111.x, e111.y, e211.x, e211.y };
 	hitbox[2].segmento[3] = { e121.x, e121.y, e111.x, e111.y };
-	
+
 }
 
 void Tentaculo::setColor(Byte r, Byte g, Byte b) {
