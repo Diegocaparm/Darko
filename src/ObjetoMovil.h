342f1130e41b5f7aa57514750dd1f1acc0933848
davidapupm <72233683+davidapupm@users.noreply.github.com>
2021-07-09 19:19:53 +0200

    Update ObjetoMovil.h

--- src/ObjetoMovil.h
+++ src/ObjetoMovil.h
@@ -8,7 +8,7 @@ protected:
 	Vector2D posicion, velocidad, aceleracion;
 	Hitbox hitbox;
 	int zonaV = 0, zonaH = 0;	//zonaV=0 abajo =1 arriba		zonaH =-1 izq = 0 dentro = 1 dcha
-	int flagdmg, flagesp, tempdmg;
+	int flagdmg = 0, flagesp = 0, tempdmg = 0;
 		//temporizadores, vida, flagdmg, flagesp, 
 		//posicion relativa, flag direccion
 public:
